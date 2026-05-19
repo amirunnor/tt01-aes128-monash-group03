d27c5cb19d9c3f85a395353c4ace5d6f6a3b74ca
amirunnor <129133914+amirunnor@users.noreply.github.com>
2026-05-19 23:12:30 +0800

    Update aes_data_path.v

--- src/aes_data_path.v
+++ src/aes_data_path.v
@@ -1,57 +1,50 @@
+module aes_data_path(
+    input  wire [7:0] d_in,
+    output wire [7:0] d_out,
+    input  wire       pld,
+    input  wire [1:0] c3,
+    input  wire       clk,
+    input  wire [7:0] mc_en,
+    input  wire [7:0] rk_delayed_out,
+    input  wire [7:0] rk_last_out
+);
 
+    wire [7:0] sr_in, sr_out, s_out, mc_out0, mc_out1, mc_out2, mc_out3, sbox_o;
+    wire [31:0] pdin;
 
+    assign pdin  = {mc_out0, mc_out1, mc_out2, mc_out3};
+    assign sr_in = rk_delayed_out ^ s_out;
+    assign d_out = sbox_o ^ rk_last_out;
 
+    // Unit Instantiations
+    byte_permutation SR (
+        .in(sr_in), 
+        .out(sr_out), 
+        .c3(c3), 
+        .clk(clk)
+    );
 
+    ps_conv PS (
+        .d_in(d_in), 
+        .s_out(s_out), 
+        .pdin(pdin), 
+        .pld(pld), 
+        .clk(clk)
+    );
 
+    mixcolumn_8 MC (
+        .sbox_o(sbox_o), 
+        .mc_en(mc_en), 
+        .mc_out0(mc_out0), 
+        .mc_out1(mc_out1), 
+        .mc_out2(mc_out2), 
+        .mc_out3(mc_out3), 
+        .clk(clk)
+    );
 
+    bSbox SB (
+        .in(sr_out), 
+        .out(sbox_o)
+    );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+endmodule
