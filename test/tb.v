14aec6759f8e82c82ce3cf0f069656ff6bf64901
amirunnor <129133914+amirunnor@users.noreply.github.com>
2026-05-19 23:34:45 +0800

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_amirunhaechan_aes128 user_project (
+  tt_um_amirun_8_aes user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
